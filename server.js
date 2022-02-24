e6f0490dccd289c28c0aab9add9abf624646e9ac
truonggiangit793 <truonggiangit793@gmail.com>
2022-02-24 14:21:36 +0700

    Update server

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-const express = require("express");
-const app = express();
-app.use(express.static("dist"));
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use("/app", express.static(path.join(__dirname, "dist")));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 module.exports = app;

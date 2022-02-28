09e038cab6ec14832ea4c4887bc6ffa206070632
truonggiangit793 <truonggiangit793@gmail.com>
2022-02-28 12:09:57 +0700

    fix sẻver bug

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "dist")));
+
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
